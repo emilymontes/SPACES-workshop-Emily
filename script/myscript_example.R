19297ea15c232a2c3b995153eb9e156c924d6ff5
Gabby Verde <Gverdegarcia@cpp.edu>
2026-07-01 14:36:12 -0700

    changed color to species and plot to geom_point.

--- script/myscript_example.R
+++ script/myscript_example.R
@@ -14,3 +14,7 @@ ggplot(complete_old, aes(x = genus, y = weight)) +
   geom_boxplot()
 
 View(complete_old)
+
+#This is where I start making changes (gabby)
+ggplot(complete_old, aes(x = hindfoot_length, y = weight, color = species))+
+  geom_point()
